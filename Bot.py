248e49225ceea44688440fc1a18fc923a35c8c7f
Davide Pellegrino s323013 <s323013@studenti.polito.it>
2024-10-01 17:56:41 +0200

    first version working with groups

--- Bot.py
+++ Bot.py
@@ -1,16 +1,23 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+from common import token_code
+from Group import groups
 # Define the command handler for the /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Ciao Davide!')
+    keyboard=[]
+    for group in groups:
+        keyboard.append([InlineKeyboardButton(group['name'], url=group['url'])])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Welcome to the Menu Bot! Please choose an group to see:', reply_markup=reply_markup)
+
 
 def main():
     # Replace 'YOUR_TOKEN' with your actual bot token
-    application = ApplicationBuilder().token("8192550968:AAG7OjregvS_WWTtDZUnX3PUCp5WatTBHvY").build()
+    application = ApplicationBuilder().token(token_code).build()
 
     # Register the /start command handler
     application.add_handler(CommandHandler('start', start))
+    
 
     # Start the Bot
     application.run_polling()
---
+++ Group.py
@@ -0,0 +1,37 @@
+# Group.py
+groups = [
+    {
+        'name': 'Softeng 2',
+        'url': 'https://t.me/joinchat/AWHhTUy1UVuIM0WvbHNrwg',
+    },
+    {
+        'name': 'Distributed Systems Programming',
+        'url': 'https://t.me/joinchat/AWHhTUy1UVuIM0WvbHNrwg',
+    },
+    {
+        'name': 'Human Computer Interaction',
+        'url': 'https://t.me/joinchat/AWHhTUX0hDr-lh0jv2ykng',
+    },
+    {
+        'name': 'Main Class Group',
+        'url': 'https://t.me/+tpFeU4udsWIyOTc8',
+    },
+]
+animes = [
+    {
+        'name': 'Naruto',
+        'url': 'https://t.me/joinchat/AWHhTUy1UVuIM0WvbHNrwg',
+    },
+    {
+        'name': 'One Piece',
+        'url': 'https://t.me/joinchat/AWHhTUy1UVuIM0WvbHNrwg',
+    },
+    {
+        'name': 'Attack on Titan',
+        'url': 'https://t.me/joinchat/AWHhTUX0hDr-lh0jv2ykng',
+    },
+    {
+        'name': 'Death Note',
+        'url': 'https://t.me/+tpFeU4udsWIyOTc8',
+    },
+]
